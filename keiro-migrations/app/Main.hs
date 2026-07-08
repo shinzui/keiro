20e4ef80444aa1ec1100fde2da4177efb28920eb
Nadeem Bitar <nadeem@gmail.com>
2026-07-08 11:39:39 -0700

    fix(migrations): use unprefixed cli config fields

--- keiro-migrations/app/Main.hs
+++ keiro-migrations/app/Main.hs
@@ -6,33 +6,28 @@ where
 import Codd (ApplyResult (SchemasNotVerified), CoddSettings, VerifySchemas (LaxCheck))
 import Codd.Extras.Cli (CheckMode (..), MigrationCliConfig (..), migrationCliMain)
 import Data.Time (DiffTime, secondsToDiffTime)
-import Keiro.Migrations (
-    migrationStatus,
-    runAllKeiroMigrations,
-    runAllKeiroMigrationsNoCheck,
-    verifySchema,
- )
-import Keiro.Migrations.New (defaultMigrationsDir, newMigrationFile)
+import Keiro.Migrations qualified as Migrations
+import Keiro.Migrations.New qualified as New
 
 main :: IO ()
 main =
     migrationCliMain
         MigrationCliConfig
-            { cliProgramName = "keiro-migrate"
-            , cliMigrationsDirEnv = "KEIRO_MIGRATIONS_DIR"
-            , cliDefaultMigrationsDir = defaultMigrationsDir
-            , cliNewMigrationFile = newMigrationFile
-            , cliRunUp = runUp
-            , cliVerifySchema = verifySchema
-            , cliMigrationStatus = migrationStatus
-            , cliConnectTimeout = secondsToDiffTime 5
-            , cliNoCheckEnv = Just "KEIRO_MIGRATE_NO_CHECK"
-            , cliEmbedRefreshHint =
+            { programName = "keiro-migrate"
+            , migrationsDirEnv = "KEIRO_MIGRATIONS_DIR"
+            , defaultMigrationsDir = New.defaultMigrationsDir
+            , newMigrationFile = New.newMigrationFile
+            , runUp = runUpMigrations
+            , verifySchema = Migrations.verifySchema
+            , migrationStatus = Migrations.migrationStatus
+            , connectTimeout = secondsToDiffTime 5
+            , noCheckEnv = Just "KEIRO_MIGRATE_NO_CHECK"
+            , embedRefreshHint =
                 "Next: touch the embed comment in src/Keiro/Migrations.hs so embedDir picks it up (or run `cabal clean`)."
             }
 
-runUp :: CheckMode -> CoddSettings -> DiffTime -> IO ApplyResult
-runUp NoCheck settings connectTimeout =
-    runAllKeiroMigrationsNoCheck settings connectTimeout >> pure SchemasNotVerified
-runUp Checked settings connectTimeout =
-    runAllKeiroMigrations settings connectTimeout LaxCheck
+runUpMigrations :: CheckMode -> CoddSettings -> DiffTime -> IO ApplyResult
+runUpMigrations NoCheck settings timeout =
+    Migrations.runAllKeiroMigrationsNoCheck settings timeout >> pure SchemasNotVerified
+runUpMigrations Checked settings timeout =
+    Migrations.runAllKeiroMigrations settings timeout LaxCheck
